e62b0a5152bf8ec4bb24f1fe0b598a9960644791
Peter Stace <peter@spaceship.com.au>
2018-11-27 18:49:56 +1100

    Set up disk partitions

--- root_install.sh
+++ root_install.sh
@@ -2,6 +2,10 @@
 
 set -exo pipefail
 
-false
-
+# NTP for time.
 timedatectl set-ntp true
+
+# Set up disk partition.
+printf 'n\n\n\n\na\nw\n' | fdisk /dev/sda
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
