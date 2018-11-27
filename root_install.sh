018dda7ec244b3a44773779864c85738f555d7a6
Peter Stace <peter@spaceship.com.au>
2018-11-27 19:00:43 +1100

    Set up mirrors

--- root_install.sh
+++ root_install.sh
@@ -9,3 +9,12 @@ timedatectl set-ntp true
 echo "type=83, bootable" | sfdisk /dev/sda
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
+
+echo '
+## Australia
+Server = http://ftp.iinet.net.au/pub/archlinux/$repo/os/$arch
+Server = http://mirror.internode.on.net/pub/archlinux/$repo/os/$arch
+Server = http://ftp.swin.edu.au/archlinux/$repo/os/$arch
+Server = http://archlinux.melbourneitmirror.net/$repo/os/$arch
+Server = http://archlinux.mirror.digitalpacific.com.au/$repo/os/$arch
+' | /etc/pacman.d/mirrorlist
