dad5843a69f5e852d74d9974d216372a881bb0fa
Peter Stace <peter@spaceship.com.au>
2018-11-27 18:52:08 +1100

    Fix quoting in fdisk command

--- root_install.sh
+++ root_install.sh
@@ -6,6 +6,6 @@ set -exo pipefail
 timedatectl set-ntp true
 
 # Set up disk partition.
-printf 'n\n\n\n\na\nw\n' | fdisk /dev/sda
+printf "n\n\n\n\na\nw\n" | fdisk /dev/sda
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
