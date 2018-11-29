36fb88de7b97ba0eb72a2b6ff63b439dc0be09a3
Peter Stace <peter@spaceship.com.au>
2018-11-30 08:26:17 +1100

    Fix some minor issues relating to SSH key uploads

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 set -exo pipefail
 
 # Set up SSH keys for Github.
-ssh-keygen
+ssh-keygen -N "" -f "$HOME/.ssh/id_rsa"
 set +x
-printf "github password > "
-read github_password
+printf "github token (must have write:public_key scope) > "
+read github_token
 set -x
 payload=$(jq -R "{title: $(hostname | jq -R .), data: .}" ~/.ssh/id_rsa.pub)
-curl -u "peterstace:$github_password" --data "$payload"
+curl -u "peterstace:$github_token" --data "$payload" "https://api.github.com/user/keys"
 
 git clone git@github.com:peterstace/dotfiles.git ~/r/dotfiles
 ~/r/dotfiles/link.sh
