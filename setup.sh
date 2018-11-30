a3c6434a38aa9281e9abb1df567460e9886b3579
Peter Stace <peter@spaceship.com.au>
2018-11-30 12:37:24 +1100

    Fixes for SSH keys

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ set +x
 printf "github token (must have write:public_key scope) > "
 read github_token
 set -x
-payload=$(jq -R "{title: $(hostname | jq -R .), data: .}" ~/.ssh/id_rsa.pub)
+payload=$(jq -R "{title: $(hostname | jq -R .), key: .}" ~/.ssh/id_rsa.pub)
 curl -u "peterstace:$github_token" --data "$payload" "https://api.github.com/user/keys"
 
 git clone git@github.com:peterstace/dotfiles.git ~/r/dotfiles
