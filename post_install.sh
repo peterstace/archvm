597b5d5f91d5bc31f9b827d9d524d79589a89ed6
Peter Stace <peter@spaceship.com.au>
2018-12-01 13:40:03 +1100

    Add missing file

---
+++ post_install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -exo pipefail
+
+systemctl enable dhcpcd.service
+systemctl enable sshd.socket
+
+pacman --noconfirm -S \
+	aws-cli \
+	base-devel \
+	clang \
+	cmake \
+	diff-so-fancy \
+	docker \
+	docker-compose \
+	dos2unix \
+	exa \
+	expect \
+	fzf \
+	genius \
+	git \
+	gnupg \
+	gnuplot \
+	go \
+	hdparm \
+	htop \
+	jq \
+	ncdu \
+	parallel \
+	pass \
+	postgresql \
+	python2 \
+	python2-setuptools \
+	python3 \
+	the_silver_searcher \
+	time \
+	tmux \
+	traceroute \
+	unzip \
+	vim
