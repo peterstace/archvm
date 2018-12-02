7d1fb4c04b128c98bdb6259e26f86a85e6497590
Peter Stace <peter@spaceship.com.au>
2018-12-03 08:09:30 +1100

    Precreate dirs inside path

--- setup.sh
+++ setup.sh
@@ -20,3 +20,8 @@ git clone git@github.com:peterstace/dotfiles.git ~/r/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 ~/.vim/bundle/YouCompleteMe/install.py
+
+# Create dirs inside PATH (so that fish doesn't complain about having stuff in
+# PATH that doesn't exist).
+mkdir -p ~/go/bin
+mkdir -p ~/bin
