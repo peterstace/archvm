f390931254d8bd22ea0c019ad4b28c7e476d2710
Peter Stace <peter@spaceship.com.au>
2019-01-04 14:23:04 +1100

    Use Plug instead of Vundle

--- setup.sh
+++ setup.sh
@@ -50,8 +50,8 @@ notice "cloning repos"
 #
 
 notice "installing vim plugins"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 ~/.vim/bundle/YouCompleteMe/install.py
 vim +GoInstallBinaries +qall
 
