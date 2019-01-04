9563baef5f93e164386e322f8a3a384f0f0e60eb
Peter Stace <peter@spaceship.com.au>
2019-01-04 17:01:45 +1100

    Update YCM install script location

--- setup.sh
+++ setup.sh
@@ -52,7 +52,7 @@ notice "cloning repos"
 notice "installing vim plugins"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
-~/.vim/bundle/YouCompleteMe/install.py
+~/.vim/plugged/YouCompleteMe/install.py
 vim +GoInstallBinaries +qall
 
 notice "installing migrate"
