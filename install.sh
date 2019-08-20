d1ca91132c9666ef81ab03d66273e1d9912803bb
Peter Stace <peterstace@gmail.com>
2019-08-21 08:34:57 +1000

    Add DisableDownloadTimeout to /etc/pacman.conf

--- install.sh
+++ install.sh
@@ -33,6 +33,9 @@ Server = http://ftp.swin.edu.au/archlinux/$repo/os/$arch
 Server = http://archlinux.melbourneitmirror.net/$repo/os/$arch
 Server = http://archlinux.mirror.digitalpacific.com.au/$repo/os/$arch
 ' > /etc/pacman.d/mirrorlist
+echo '
+DisableDownloadTimeout
+' >> /etc/pacman.conf
 pacstrap /mnt base
 genfstab -U /mnt >> /mnt/etc/fstab
 
