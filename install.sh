42307d0a38503ad8739bdcbfdbf2a86e16ab2408
Peter Stace <peterstace@gmail.com>
2019-08-21 09:13:08 +1000

    Revert "Add DisableDownloadTimeout to /etc/pacman.conf"
    This reverts commit d1ca91132c9666ef81ab03d66273e1d9912803bb.

--- install.sh
+++ install.sh
@@ -33,9 +33,6 @@ Server = http://ftp.swin.edu.au/archlinux/$repo/os/$arch
 Server = http://archlinux.melbourneitmirror.net/$repo/os/$arch
 Server = http://archlinux.mirror.digitalpacific.com.au/$repo/os/$arch
 ' > /etc/pacman.d/mirrorlist
-echo '
-DisableDownloadTimeout
-' >> /etc/pacman.conf
 pacstrap /mnt base
 genfstab -U /mnt >> /mnt/etc/fstab
 
