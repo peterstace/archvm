cceec3acdb1e71562fb0f573ce479b39c11084c9
Peter Stace <peterstace@gmail.com>
2019-11-07 09:03:05 +1100

    Minor tweak to grub-install

--- chroot.sh
+++ chroot.sh
@@ -26,11 +26,8 @@ echo "
 notice "installing grub"
 pacman --noconfirm -S fish grub efibootmgr openssh sudo wget
 
-grub-install /dev/sda --target=x86_64-efi --efi-directory=/boot
+grub-install --removable --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-
-sudo mkdir -p /boot/EFI/BOOT/
-sudo cp /boot/EFI/arch/grubx64.efi /boot/EFI/BOOT/BOOTX64.EFI
 
 notice "setting up root password"
 echo "root:root" | chpasswd
