de232a053df13e7d2b045368850c014bc29cfdde
Peter Stace <peterstace@gmail.com>
2019-11-11 08:45:38 +1100

    Revert "Don't need to genfstab for boot"
    This reverts commit 0ba96fe5d8ca0d2d4c4f01d420b96b9c2edcae11.

--- install.sh
+++ install.sh
@@ -43,6 +43,7 @@ sed -i "$(echo $(sed -n '/\[options\]/=' /etc/pacman.conf) + 1 | bc)iDisableDown
 
 pacstrap /mnt base
 genfstab -U -p /mnt >> /mnt/etc/fstab
+genfstab -U -p /mnt/boot >> /mnt/etc/fstab
 
 notice "entering chroot"
 arch-chroot /mnt /archvm/chroot.sh
